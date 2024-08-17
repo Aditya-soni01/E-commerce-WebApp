6f6f784d92ee4b68514d3168fbe3ac90d755d51b
Aditya Soni <adityasoni01199@gmail.com>
2024-08-17 23:43:39 +0530

    correct vercel.json file

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+# Install the required packages from requirements.txt
+python3.10 -m pip install -r requirements.txt
+# Collect static files and clear out the existing ones
+python3.10 manage.py collectstatic --noinput --clear
 echo "BUILD END"
