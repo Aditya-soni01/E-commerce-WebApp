e9115a49a7d4fb85fa9e329a85ebdd1c9a8084b9
Aditya Soni <adityasoni01199@gmail.com>
2024-08-17 23:08:19 +0530

    done setting for vercel

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!tya(hg^+mf0m+!d8dqeixgu98@j8r_s6d1=z%b3kjcmoq6nxr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -133,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROORT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
